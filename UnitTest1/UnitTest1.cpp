7eacdd3c9e699a58f280460f5edea388f99d786a
igor4ikc <igor4ikc@users.noreply.github.com>
2021-03-30 23:57:12 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 2.5/Time.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Time a(1, 2, 3);
+			++a;
+			int t = a.getF();
+			Assert::AreEqual(t, 2);
+
+		}
+	};
+}
